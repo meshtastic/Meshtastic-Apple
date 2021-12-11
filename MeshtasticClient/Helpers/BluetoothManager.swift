98368640c9ff874ae1e165ea2f539899f7e79bbc
Garth Vander Houwen <garthvh@yahoo.com>
2021-12-10 17:44:28 -0800

    V 1.8.3

---
+++ MeshtasticClient/Helpers/BluetoothManager.swift
@@ -0,0 +1,27 @@
+//
+//  BluetoothManager.swift
+//  MeshtasticClient
+//
+//  Created by Garth Vander Houwen on 12/1/21.
+//
+
+import Combine
+import CoreBluetooth
+
+final class BluetoothManager: NSObject {
+	
+	private var centralManager: CBCentralManager!
+	
+	var stateSubject: PassthroughSubject<CBManagerState, Never> = .init()
+	var peripheralSubject: PassthroughSubject<CBPeripheral, Never> = .init()
+	
+	func start() {
+		centralManager = .init(delegate: self, queue: .main)
+	}
+	
+	func connect(_ peripheral: CBPeripheral) {
+		centralManager.stopScan()
+		peripheral.delegate = self
+		centralManager.connect(peripheral)
+	}
+}
--- MeshtasticClient/Helpers/HelperFunctions.swift
+++
@@ -1,9 +0,0 @@
-//
-//  HelperFunctions.swift
-//  MeshtasticClient
-//
-//  Created by Garth Vander Houwen on 9/14/21.
-//
-
-import Foundation
-import SwiftUI
