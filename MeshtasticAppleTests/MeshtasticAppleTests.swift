50ecbf2015327ca45414991d1f604faf3c7ed70b
Garth Vander Houwen <garthvh@yahoo.com>
2022-06-09 21:48:33 -0700

    Rename all project settings so I can edit xcodeproj file and fix path for core data

--- MeshtasticAppleTests/MeshtasticAppleTests.swift
+++ MeshtasticAppleTests/MeshtasticAppleTests.swift
@@ -8,14 +8,17 @@
 import XCTest
 // @testable import MeshtasticClient
 
-class MeshtasticClientTests: XCTestCase {
+class MeshtasticAppleTests: XCTestCase {
 
     override func setUpWithError() throws {
+		super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDownWithError() throws {
+		
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+		super.tearDown()
     }
 
     func testExample() throws {
