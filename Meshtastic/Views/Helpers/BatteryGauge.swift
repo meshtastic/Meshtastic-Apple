9883afe958a213a7dc5188edb649d880ba70f6a1
Garth Vander Houwen <garthvh@yahoo.com>
2022-09-28 17:09:35 -0700

    Finish battery gauge, delete old battery icon view

--- Meshtastic/Views/Helpers/BatteryGauge.swift
+++ Meshtastic/Views/Helpers/BatteryGauge.swift
@@ -2,7 +2,7 @@
 //  BatteryGauge.swift
 //  Meshtastic
 //
-//  Created by Garth Vander Houwen on 9/28/22.
+//  Copyright(c) Garth Vander Houwen 9/28/22.
 //
 
 import SwiftUI
@@ -26,16 +26,17 @@ struct BatteryGauge: View {
 					.symbolRenderingMode(.hierarchical)
 			} else {
 				
+				let gradient = Gradient(colors: [.red, .orange, .green])
 				Gauge(value: batteryLevel, in: minValue...maxValue) {
-					if batteryLevel > 1.0 && batteryLevel <= 9 {
+					if batteryLevel > 1.0 && batteryLevel < 10 {
 						Label("Battery Level %", systemImage: "battery.0")
-					} else if batteryLevel > 10.0 && batteryLevel <= 25.00 {
+					} else if batteryLevel >= 10.0 && batteryLevel < 25.00 {
 						Label("Battery Level %", systemImage: "battery.25")
-					} else if batteryLevel > 26.0 && batteryLevel <= 50.00 {
+					} else if batteryLevel >= 25.0 && batteryLevel < 50.00 {
 						Label("Battery Level %", systemImage: "battery.50")
-					} else if batteryLevel > 51.0 && batteryLevel <= 75.00 {
+					} else if batteryLevel >= 50.0 && batteryLevel < 75.00 {
 						Label("Battery Level %", systemImage: "battery.75")
-					} else if batteryLevel > 76.0 && batteryLevel <= 99.00 {
+					} else if batteryLevel >= 75.0 && batteryLevel <= 99.00 {
 						Label("Battery Level %", systemImage: "battery.100")
 					} else {
 						Label("Battery Level %", systemImage: "battery.100.bolt")
@@ -43,9 +44,24 @@ struct BatteryGauge: View {
 				} currentValueLabel: {
 					Text(Int(batteryLevel), format: .percent)
 				}
-				.tint(.green)
+				.tint(gradient)
 				.gaugeStyle(.accessoryCircular)
 			}
+		}
+	}
+}
+
+struct BatteryGauge_Previews: PreviewProvider {
+	static var previews: some View {
+		
+		VStack {
+			BatteryGauge(batteryLevel: 0.0)
+			BatteryGauge(batteryLevel: 9.0)
+			BatteryGauge(batteryLevel: 24.0)
+			BatteryGauge(batteryLevel: 49.0)
+			BatteryGauge(batteryLevel: 74.0)
+			BatteryGauge(batteryLevel: 99.0)
+			BatteryGauge(batteryLevel: 100.0)
 		}
 	}
 }
