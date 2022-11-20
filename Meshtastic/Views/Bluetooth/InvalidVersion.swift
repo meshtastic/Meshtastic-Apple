537ad1bf738123bc92c59a0185b0c4b1b730dcae
Garth Vander Houwen <garthvh@yahoo.com>
2022-11-20 09:50:18 -0800

    Update invalid firmware view

--- Meshtastic/Views/Bluetooth/InvalidVersion.swift
+++ Meshtastic/Views/Bluetooth/InvalidVersion.swift
@@ -17,27 +17,21 @@ struct InvalidVersion: View {
 		
 		VStack {
 			
-			Text("Update your firmware")
+			Text("Firmware Update Required")
 				.font(.largeTitle)
 				.foregroundColor(.orange)
 			
 			Divider()
-			
 			VStack {
-			
-					
 				Text("The Meshtastic Apple apps support firmware version \(minimumVersion) and above.")
 						.font(.title2)
 						.padding(.bottom)
-				
 				Link("Firmware update docs", destination: URL(string: "https://meshtastic.org/docs/getting-started/flashing-firmware/")!)
 					.font(.title)
 					.padding()
-				
 				Link("Additional help", destination: URL(string: "https://meshtastic.org/docs/faq")!)
 					.font(.title)
 					.padding()
-				
 			}
 			.padding()
 				
@@ -52,10 +46,6 @@ struct InvalidVersion: View {
 					.padding(.bottom)
 				
 				Text("Version \(minimumVersion) includes breaking changes to devices and the client apps. Only nodes version \(minimumVersion) and above are supported.")
-					.font(.callout)
-					.padding([.leading, .trailing, .bottom])
-				
-				Text("There is a build for 1.2 EOL under Other Versions in TestFlight that will be available until the end of November 2022.")
 					.font(.callout)
 					.padding([.leading, .trailing, .bottom])
 				
