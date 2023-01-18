2d59dc7d7306b56372b29e1592940bc260a7f84b
Garth Vander Houwen <garthvh@yahoo.com>
2023-01-18 06:57:23 -0800

    Updated gen_protos

--- gen_protos.sh
+++ gen_protos.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # simple sanity checking for repo
-if [ ! -d "../Meshtastic-protobufs" ]; then
-  echo "Please check out the https://github.com/meshtastic/Meshtastic-protobufs the parent directory."
+if [ ! -d "../protobufs/meshtastic" ]; then
+  echo "Please check out the https://github.com/meshtastic/protobufs parent directory."
   exit
 fi
 
@@ -12,15 +12,15 @@ if [ ! -x "`which protoc`" ]; then
   exit
 fi
 
-pdir=$(realpath "../Meshtastic-protobufs")
+pdir=$(realpath "../protobufs/meshtastic")
 sdir=$(realpath "./Meshtastic/Protobufs")
 echo "pdir:$pdir sdir:$sdir"
-pfiles="admin.proto apponly.proto cannedmessages.proto channel.proto config.proto device_metadata.proto deviceonly.proto localonly.proto mesh.proto module_config.proto mqtt.proto portnums.proto remote_hardware.proto 
-storeforward.proto telemetry.proto"
+pfiles="admin.proto apponly.proto cannedmessages.proto channel.proto config.proto device_metadata.proto deviceonly.proto localonly.proto mesh.proto module_config.proto mqtt.proto portnums.proto remote_hardware.proto rtttl.proto storeforward.proto telemetry.proto xmodem.proto"
 for pf in $pfiles
 do
   echo "Generating $pf..."
   protoc --swift_out=${sdir} --proto_path=${pdir} $pf
+  echo protoc --swift_out=${sdir} --proto_path=${pdir} $pf
 done
 echo "Done generating the swift files from the proto files."
 echo "Build, test, and commit changes."
