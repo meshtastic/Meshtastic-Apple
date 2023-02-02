9705e82b9740b136d205eb27755b60bb6268257b
Jon <jcw122@gmail.com>
2023-02-02 10:04:08 -0500

    Updated roles

--- Meshtastic/Enums/DeviceRoles.swift
+++ Meshtastic/Enums/DeviceRoles.swift
@@ -46,13 +46,13 @@ enum DeviceRoles: Int, CaseIterable, Identifiable {
 			case .clientMute:
 				return NSLocalizedString("device.role.clientmute", comment: "Client Mute - Same as a client except packets will not hop over this node, does not contribute to routing packets for mesh.")
 			case .router:
-				return NSLocalizedString("device.role.router", comment: "Router -  Mesh packets will prefer to be routed over this node. This node will not be used by client apps. The wifi/ble radios and the oled screen will be put to sleep.")
+				return NSLocalizedString("device.role.router", comment: "Router -  Mesh packets will prefer to be routed over this node. Assumes device will operate in a standalone manner while placed in a location with a coverage advantage. WARNING: The BLE/Wi-Fi radios and the OLED screen will be put to sleep.")
 			case .routerClient:
-				return NSLocalizedString("device.role.routerclient", comment: "Router Client - Mesh packets will prefer to be routed over this node. The Router Client can be used as both a Router and an app connected Client.")
+				return NSLocalizedString("device.role.routerclient", comment: "Router Client - Hybrid of the Client and Router roles. Similar to Router, except the Router Client can be used as both a Router and an app connected Client. BLE/Wi-Fi and OLED screen will not be put to sleep.")
 			case .repeater:
-				return "Mesh packets will simply be rebroadcasted over this node. Nodes configured with this role will not originate NodeInfo, Position, Telemetry or any other packet type. They will simply rebroadcast any mesh packets on the same frequency, channel num, spread factor, and coding rate."
+				return "Repeater - Mesh packets will prefer to be routed over this node. This role eliminates unnecessary overhead such as NodeInfo, DeviceTelemetry, and any other mesh packet, resulting in the device not appearing as part of the network.  Please see Rebroadcast Mode for additional settings specific to this role."
 			case .tracker:
-				return "Position Mesh packets will be prioritized higher and sent more frequently by default."
+				return "Tracker - For use with devices intended as a GPS tracker. Position packets sent from this device will be higher priority, with position broadcasting every two minutes. Smart Position Broadcast will default to off."
 			}
 		}
 	}
