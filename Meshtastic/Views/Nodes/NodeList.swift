58a90559bcdcce294b337054a20c68434513a7fd
Garth Vander Houwen <garthvh@yahoo.com>
2023-05-16 15:15:58 -0700

    Node list battery indicator

--- Meshtastic/Views/Nodes/NodeList.swift
+++ Meshtastic/Views/Nodes/NodeList.swift
@@ -37,8 +37,15 @@ struct NodeList: View {
 						let connected: Bool = (bleManager.connectedPeripheral != nil && bleManager.connectedPeripheral?.num ?? -1 == node.num)
 						VStack(alignment: .leading) {
 							HStack {
-								CircleText(text: node.user?.shortName ?? "???", color: Color(UIColor(hex: UInt32(node.num))), circleSize: 65, fontSize: (node.user?.shortName ?? "???").isEmoji() ? 44 : 22, brightness: 0.0, textColor: UIColor(hex: UInt32(node.num)).isLight() ? .black : .white)
-									.padding(.trailing, 5)
+								VStack(alignment: .leading) {
+									CircleText(text: node.user?.shortName ?? "???", color: Color(UIColor(hex: UInt32(node.num))), circleSize: 65, fontSize: (node.user?.shortName ?? "???").isEmoji() ? 44 : 22, brightness: 0.0, textColor: UIColor(hex: UInt32(node.num)).isLight() ? .black : .white)
+										.padding(.trailing, 5)
+									let deviceMetrics = node.telemetries?.filtered(using: NSPredicate(format: "metricsType == 0"))
+									if deviceMetrics?.count ?? 0 >= 1 {
+										let mostRecent = deviceMetrics?.lastObject as? TelemetryEntity
+										BatteryLevelCompact(batteryLevel: mostRecent?.batteryLevel, font: .caption2, iconFont: .callout, color: .accentColor)
+									}
+								}
 								VStack(alignment: .leading) {
 									Text(node.user?.longName ?? "unknown".localized)
 										.fontWeight(.medium)
