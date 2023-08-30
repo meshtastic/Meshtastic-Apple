176ba630c609164faae9844ce8e6a6eb8fac0d5d
Garth Vander Houwen <garthvh@yahoo.com>
2023-08-29 22:12:51 -0700

    Three column messaging test

---
+++ Meshtastic/Views/Messages/Messages.swift
@@ -0,0 +1,96 @@
+//
+//  Messages.swift
+//  Meshtastic
+//
+//  Copyright(c) Garth Vander Houwen 8/29/23.
+//
+
+import SwiftUI
+import CoreData
+
+struct Messages: View {
+
+	@Environment(\.managedObjectContext) var context
+	@EnvironmentObject var bleManager: BLEManager
+
+	@FetchRequest(
+		sortDescriptors: [NSSortDescriptor(key: "lastMessage", ascending: false), NSSortDescriptor(key: "longName", ascending: true)],
+		animation: .default)
+
+	private var users: FetchedResults<UserEntity>
+	@State var node: NodeInfoEntity?
+	@State private var userSelection: UserEntity? // Nothing selected by default.
+	@State private var channelSelection: ChannelEntity? // Nothing selected by default.
+	@State private var isPresentingDeleteChannelMessagesConfirm: Bool = false
+	@State private var isPresentingDeleteUserMessagesConfirm: Bool = false
+	@State private var isPresentingTraceRouteSentAlert = false
+	
+	@State private var columnVisibility = NavigationSplitViewVisibility.all
+	
+	enum MessagesSidebar {
+		case groupMessages
+		case directMessages
+	}
+
+	var body: some View {
+
+		NavigationSplitView(columnVisibility: $columnVisibility) {
+			// Sidebar
+			List {
+				NavigationLink {
+					ChannelList(node: node)
+				} label: {
+					Image(systemName: "person.3")
+						.symbolRenderingMode(.hierarchical)
+						.foregroundColor(.accentColor)
+						.brightness(0.2)
+					Text("channels")
+						.font(.title2)
+				}
+				NavigationLink {
+					UserList(node: node)
+				} label: {
+					Image(systemName: "person")
+						.symbolRenderingMode(.hierarchical)
+						.foregroundColor(.accentColor)
+						.brightness(0.2)
+					Text("direct.messages")
+						.font(.title2)
+				}
+			}
+			.navigationTitle("messages")
+			.navigationBarItems(leading: MeshtasticLogo())
+			.onAppear {
+				self.bleManager.context = context
+				if UserDefaults.preferredPeripheralId.count > 0 {
+					let fetchNodeInfoRequest: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest.init(entityName: "NodeInfoEntity")
+					fetchNodeInfoRequest.predicate = NSPredicate(format: "num == %lld", Int64(bleManager.connectedPeripheral?.num ?? -1))
+					do {
+						guard let fetchedNode = try context.fetch(fetchNodeInfoRequest) as? [NodeInfoEntity] else {
+							return
+						}
+						// Found a node, check it for a region
+						if !fetchedNode.isEmpty {
+							node = fetchedNode[0]
+						}
+					} catch {
+
+					}
+				}
+			}
+			
+		} content: {
+			
+			ChannelList()
+			UserList()
+
+		} detail: {
+			if let user = userSelection {
+				UserMessageList(user: user)
+
+			} else {
+				Text("select.contact")
+			}
+		}
+	}
+}
