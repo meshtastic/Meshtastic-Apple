de57298a8bcd38b39a88ea63ac709233cdb0b384
Garth Vander Houwen <garthvh@yahoo.com>
2023-09-12 13:29:21 -0700

    Base Map Lines

--- Meshtastic/Views/Nodes/Helpers/NodeMapSwiftUI.swift
+++ Meshtastic/Views/Nodes/Helpers/NodeMapSwiftUI.swift
@@ -13,12 +13,13 @@ import WeatherKit
 @available(iOS 17.0, *)
 struct NodeMapSwiftUI: View {
 	
-	@Namespace var mapScope
 	@Environment(\.managedObjectContext) var context
 	@EnvironmentObject var bleManager: BLEManager
+	/// Map State
+	@Namespace var mapScope
 	@AppStorage("meshMapType") private var meshMapType = 0
 	@AppStorage("meshMapShowNodeHistory") private var showNodeHistory = false
-	@AppStorage("meshMapShowRouteLines") private var meshMapShowRouteLines = false
+	@AppStorage("meshMapShowRouteLines") private var showRouteLines = false
 	@State private var selectedMapLayer: MapLayer = .standard
 	@State var waypointCoordinate: WaypointCoordinate?
 	@State var editingWaypoint: Int = 0
@@ -45,88 +46,84 @@ struct NodeMapSwiftUI: View {
 		if mostRecent != nil {
 			NavigationStack {
 				ZStack {
-					Map(initialPosition: .camera(MapCamera(centerCoordinate: mostRecent!.coordinate, distance: 500, heading: 90, pitch: 60)),
-						bounds: MapCameraBounds(minimumDistance: 100, maximumDistance: 3500),
+					Map(initialPosition: .camera(MapCamera(centerCoordinate: mostRecent!.coordinate, distance: 1000, heading: 0, pitch: 60)),
+						bounds: MapCameraBounds(minimumDistance: 100, maximumDistance: .infinity),
 						scope: mapScope) {
 						/// Route Lines
-						if meshMapShowRouteLines {
+						if showRouteLines {
 							MapPolyline(coordinates: lineCoords, contourStyle: .straight)
-								.stroke(Color(nodeColor.lighter()), lineWidth: 8)
+								.stroke(Color(nodeColor.lighter()), lineWidth: 6)
 						}
 						/// Node Annotations
 						ForEach(positionArray.reversed(), id: \.id) { position in
-								Annotation(position.latest ? node.user?.shortName ?? "?" : "", coordinate:  position.coordinate) {
-									ZStack {
-										
-										let pf = PositionFlags(rawValue: Int(position.nodePosition?.metadata?.positionFlags ?? 3))
-										
-										let symbolName = "flipphone"
-										
-										if position.latest {
-											Circle()
-												.foregroundStyle(Color(nodeColor).opacity(0.4))
+							let pf = PositionFlags(rawValue: Int(position.nodePosition?.metadata?.positionFlags ?? 3))
+							let formatter = MeasurementFormatter()
+							let speedText = formatter.string(from: Measurement(value: Double(position.speed), unit: UnitSpeed.kilometersPerHour))
+							Annotation(position.latest ? node.user?.shortName ?? "?" : (pf.contains(.Speed) && position.speed > 2) ? speedText : "", coordinate:  position.coordinate) {
+								ZStack {
+									if position.latest {
+										Circle()
+											.foregroundStyle(Color(nodeColor.lighter()).opacity(0.4))
 												.frame(width: 60, height: 60)
-												
-											Image(systemName: symbolName)
+										
+										if pf.contains(.Heading) {
+											Image(systemName: pf.contains(.Speed) && position.speed > 1 ? "location.north.fill" : "hexagon")
 												.symbolEffect(.pulse.byLayer)
-												.padding(7)
-												.foregroundStyle(Color(nodeColor.lighter()).isLight() ? .black : .white)
+												.padding(5)
+												.foregroundStyle(Color(nodeColor).isLight() ? .black : .white)
 												.background(Color(UIColor(hex: UInt32(node.num)).darker()))
 												.clipShape(Circle())
-												.zIndex(100)
+												.rotationEffect(.degrees(Double(position.heading)))
 										} else {
-											if showNodeHistory {
-												if pf.contains(.Heading) {
-//													if parent.userTrackingMode != MKUserTrackingMode.followWithHeading {
-//														annotationView.glyphImage = UIImage(systemName: "location.north.fill")?
-//														subtitle.text! += "Heading: \(String(positionAnnotation.heading)) \n"
-//													} else {
-//														annotationView.glyphImage = UIImage(systemName: "flipphone")
-//													}
-												}
-												if pf.contains(.Speed) {
-//													let formatter = MeasurementFormatter()
-//													formatter.locale = Locale.current
-//													if positionAnnotation.speed <= 1 {
-//														annotationView.glyphImage = UIImage(systemName: "hexagon")
-//													}
-//													subtitle.text! += "Speed: \(formatter.string(from: Measurement(value: Double(positionAnnotation.speed), unit: UnitSpeed.kilometersPerHour))) \n"
-												}
-												
-												
+											Image(systemName: "flipphone")
+												.symbolEffect(.pulse.byLayer)
+												.padding(5)
+												.foregroundStyle(Color(nodeColor).isLight() ? .black : .white)
+												.background(Color(UIColor(hex: UInt32(node.num)).darker()))
+												.clipShape(Circle())
+										}
+									} else {
+										if showNodeHistory {
+											if pf.contains(.Heading) {
+												Image(systemName: pf.contains(.Speed) && position.speed > 0 ? "location.north.fill" : "hexagon")
+													.padding(2)
+													.foregroundStyle(Color(UIColor(hex: UInt32(node.num)).lighter()).isLight() ? .black : .white)
+													.background(Color(UIColor(hex: UInt32(node.num)).lighter()))
+													.clipShape(Circle())
+													.rotationEffect(.degrees(Double(position.heading)))
+											} else {
 												Image(systemName: "mappin.circle")
 													.padding(2)
 													.foregroundStyle(Color(UIColor(hex: UInt32(node.num)).lighter()).isLight() ? .black : .white)
 													.background(Color(UIColor(hex: UInt32(node.num)).lighter()))
 													.clipShape(Circle())
-													.zIndex(1000)
 											}
 										}
-										
 									}
 								}
-								.tag(node.num)
 							}
+							.tag(node.num)
 						}
-						.mapScope(mapScope)
-						.mapStyle(.imagery(elevation: .realistic))
-						.mapControls {
-							MapScaleView(scope: mapScope)
-								.mapControlVisibility(.visible)
-							MapUserLocationButton(scope: mapScope)
-								.mapControlVisibility(.visible)
-							MapPitchToggle(scope: mapScope)
-								.mapControlVisibility(.visible)
-						#if targetEnvironment(macCatalyst)
-							MapZoomStepper(scope: mapScope)
-								.mapControlVisibility(.visible)
-							MapPitchSlider(scope: mapScope)
-								.mapControlVisibility(.visible)
-						#endif
-							MapCompass(scope: mapScope)
-								.mapControlVisibility(.visible)
-						}
-						.controlSize(.regular)
+					}
+					.mapScope(mapScope)
+					.mapStyle(.imagery(elevation: .realistic))
+					.mapControls {
+						MapScaleView(scope: mapScope)
+							.mapControlVisibility(.visible)
+						MapUserLocationButton(scope: mapScope)
+							.mapControlVisibility(.visible)
+						MapPitchToggle(scope: mapScope)
+							.mapControlVisibility(.visible)
+					#if targetEnvironment(macCatalyst)
+						MapZoomStepper(scope: mapScope)
+							.mapControlVisibility(.visible)
+						MapPitchSlider(scope: mapScope)
+							.mapControlVisibility(.visible)
+					#endif
+						MapCompass(scope: mapScope)
+							.mapControlVisibility(.visible)
+					}
+					.controlSize(.regular)
 				}
 				.navigationBarTitle(String("Node Map " + (node.user?.shortName ?? "unknown".localized)), displayMode: .inline)
 				.navigationBarItems(trailing:
