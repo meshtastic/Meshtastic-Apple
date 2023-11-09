0c709219356caef3b9eac4d8ca27f333116e4a3b
Garth Vander Houwen <garthvh@yahoo.com>
2023-11-08 23:29:16 -0800

    Show heading and speed by default in the position popover

--- Meshtastic/Views/Nodes/Helpers/PositionPopover.swift
+++ Meshtastic/Views/Nodes/Helpers/PositionPopover.swift
@@ -14,7 +14,7 @@ struct PositionPopover: View {
 	var body: some View {
 		VStack (alignment: .leading) {
 			HStack {
-				CircleText(text: position.nodePosition?.user?.shortName ?? "?", color: Color(UIColor(hex: UInt32(position.nodePosition?.user?.num ?? 0))), circleSize: 75)
+				CircleText(text: position.nodePosition?.user?.shortName ?? "?", color: Color(UIColor(hex: UInt32(position.nodePosition?.user?.num ?? 0))), circleSize: 65)
 				Spacer()
 				Text(position.nodePosition?.user?.longName ?? "Unknown")
 					.font(.largeTitle)
@@ -82,34 +82,31 @@ struct PositionPopover: View {
 						.padding(.bottom, 5)
 					}
 					/// Heading
-					if pf.contains(.Heading) {
-						let degrees = Angle.degrees(Double(position.heading))
-						Label {
-							let heading = Measurement(value: degrees.degrees, unit: UnitAngle.degrees)
-							Text("Heading: \(heading.formatted())")
-								.foregroundColor(.primary)
-						} icon: {
-							Image(systemName: "location.north")
-								.symbolRenderingMode(.hierarchical)
-								.frame(width: 35)
-								.rotationEffect(degrees)
-						}
-						.padding(.bottom, 5)
+					let degrees = Angle.degrees(Double(position.heading))
+					Label {
+						let heading = Measurement(value: degrees.degrees, unit: UnitAngle.degrees)
+						Text("Heading: \(heading.formatted())")
+							.foregroundColor(.primary)
+					} icon: {
+						Image(systemName: "location.north")
+							.symbolRenderingMode(.hierarchical)
+							.frame(width: 35)
+							.rotationEffect(degrees)
 					}
+					.padding(.bottom, 5)
 					/// Speed
-					if pf.contains(.Speed) {
-						let formatter = MeasurementFormatter()
-						Label {
-							Text("Speed: \(formatter.string(from: Measurement(value: Double(position.speed), unit: UnitSpeed.kilometersPerHour)))")
-							//		.font(.footnote)
-								.foregroundColor(.primary)
-						} icon: {
-							Image(systemName: "gauge.with.dots.needle.33percent")
-								.symbolRenderingMode(.hierarchical)
-								.frame(width: 35)
-						}
-						.padding(.bottom, 5)
+					let formatter = MeasurementFormatter()
+					Label {
+						Text("Speed: \(formatter.string(from: Measurement(value: Double(position.speed), unit: UnitSpeed.kilometersPerHour)))")
+						//		.font(.footnote)
+							.foregroundColor(.primary)
+					} icon: {
+						Image(systemName: "gauge.with.dots.needle.33percent")
+							.symbolRenderingMode(.hierarchical)
+							.frame(width: 35)
 					}
+					.padding(.bottom, 5)
+					
 					/// Distance
 					if LocationHelper.currentLocation.distance(from: LocationHelper.DefaultLocation) > 0.0 {
 						let metersAway = position.coordinate.distance(from: LocationHelper.currentLocation)
@@ -133,7 +130,7 @@ struct PositionPopover: View {
 			}
 			.padding(.top)
 		}
-		.presentationDetents([.fraction(0.4), .medium])
+		.presentationDetents([.fraction(0.45), .medium])
 		.presentationDragIndicator(.visible)
 	}
 }
