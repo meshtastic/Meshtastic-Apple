cff96bd37c6f64573a9552681a26ce7ae6b2ea54
Garth Vander Houwen <garthvh@yahoo.com>
2024-01-19 10:56:57 -0800

    Remove spaces from saved RTTL value

--- Meshtastic/Views/Settings/Config/Module/RtttlConfig.swift
+++ Meshtastic/Views/Settings/Config/Module/RtttlConfig.swift
@@ -96,7 +96,7 @@ struct RtttlConfig: View {
 
 				let connectedNode = getNodeInfo(id: bleManager.connectedPeripheral.num, context: context)
 					if connectedNode != nil {
-						let adminMessageId =  bleManager.saveRtttlConfig(ringtone: ringtone, fromUser: connectedNode!.user!, toUser: node!.user!, adminIndex: connectedNode?.myInfo?.adminIndex ?? 0)
+						let adminMessageId =  bleManager.saveRtttlConfig(ringtone: ringtone.trimmingCharacters(in: .whitespacesAndNewlines), fromUser: connectedNode!.user!, toUser: node!.user!, adminIndex: connectedNode?.myInfo?.adminIndex ?? 0)
 						if adminMessageId > 0 {
 							// Should show a saved successfully alert once I know that to be true
 							// for now just disable the button after a successful save
