7735628a3bfa9848cc430a1a38a238dcb289f62e
Garth Vander Houwen <garthvh@yahoo.com>
2024-02-27 17:15:24 -0800

    REmove precision bits from node map

--- Meshtastic/Views/Nodes/Helpers/Map/NodeMapSwiftUI.swift
+++ Meshtastic/Views/Nodes/Helpers/Map/NodeMapSwiftUI.swift
@@ -109,16 +109,16 @@ struct NodeMapSwiftUI: View {
 //										.foregroundStyle(Color(nodeColor).opacity(0.25))
 //								}
 //							}
-							if position.latest {
-								if 11...16 ~= position.precisionBits {
-									let pp = PositionPrecision(rawValue: Int(position.precisionBits))
-									let radius : CLLocationDistance = pp?.precisionMeters ?? 0
-									if radius > 0.0 {
-										MapCircle(center: position.coordinate, radius: radius)
-											.foregroundStyle(Color(nodeColor).opacity(0.25))
-									}
-								}
-							}
+//							if position.latest {
+//								if 11...16 ~= position.precisionBits {
+//									let pp = PositionPrecision(rawValue: Int(position.precisionBits))
+//									let radius : CLLocationDistance = pp?.precisionMeters ?? 0
+//									if radius > 0.0 {
+//										MapCircle(center: position.coordinate, radius: radius)
+//											.foregroundStyle(Color(nodeColor).opacity(0.25))
+//									}
+//								}
+//							}
 							Annotation(position.latest ? node.user?.shortName ?? "?": "", coordinate: position.coordinate) {
 								LazyVStack {
 									if position.latest {
