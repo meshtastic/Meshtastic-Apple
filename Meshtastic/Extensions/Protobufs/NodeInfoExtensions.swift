e287ad6b78b34ef166187f2b6c0a09e684baca8a
Blake McAnally <bwm003@gmail.com>
2024-06-06 15:09:18 -0500

    Refactor how mesh packets are persisted to the data model

---
+++ Meshtastic/Extensions/Protobufs/NodeInfoExtensions.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+extension NodeInfo {
+	var isValidPosition: Bool {
+		hasPosition &&
+			position.longitudeI != 0 &&
+			position.latitudeI != 0 &&
+			position.latitudeI != 373346000 &&
+			position.longitudeI != -1220090000
+	}
+}
