dfc9cb32a245639786e1d7627b80baeea4bf6f25
Garth Vander Houwen <garthvh@yahoo.com>
2024-07-06 19:46:40 -0700

    Bump version

--- Meshtastic/Views/Settings/Firmware.swift
+++ Meshtastic/Views/Settings/Firmware.swift
@@ -13,7 +13,7 @@ struct Firmware: View {
 	@Environment(\.managedObjectContext) var context
 	@EnvironmentObject var bleManager: BLEManager
 	var node: NodeInfoEntity?
-	@State var minimumVersion = "2.3.9"
+	@State var minimumVersion = "2.3.15"
 	@State var version = ""
 	@State private var currentDevice: DeviceHardware?
 	@State private var latestStable: FirmwareRelease?
