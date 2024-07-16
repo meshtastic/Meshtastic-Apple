9fb3eedb9e8a5888e71ed9c977ae05e4e627a948
Blake McAnally <bwm003@gmail.com>
2024-07-15 22:48:26 -0500

    Prune several duplicate images from the asset catalog, and update to use single scale assets for most cases

--- Meshtastic/Views/Nodes/Helpers/NodeInfoItem.swift
+++ Meshtastic/Views/Nodes/Helpers/NodeInfoItem.swift
@@ -29,7 +29,7 @@ struct NodeInfoItem: View {
 			if let user = node.user {
 				VStack(alignment: .center) {
 					if user.hwModel != "UNSET" {
-						Image(user.hwModel ?? "unset".localized)
+						Image(user.hardwareImage ?? "UNSET")
 							.resizable()
 							.aspectRatio(contentMode: .fit)
 							.frame(width: 75, height: 75)
