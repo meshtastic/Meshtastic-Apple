f861e1b5115809f875cfb45b15f309d9f6f670c0
radovan <radovan@radovans-MacBook-Air.local>
2024-07-27 09:39:31 +0200

    add node list summary

--- Meowtastic/Views/Nodes/NodeList.swift
+++ Meowtastic/Views/Nodes/NodeList.swift
@@ -3,18 +3,30 @@ import OSLog
 
 struct NodeList: View {
 	@SceneStorage("selectedDetailView")
-	var selectedDetailView: String?
+	private var selectedDetailView: String?
 	@Environment(\.managedObjectContext)
-	var context
+	private var context
 	@EnvironmentObject
-	var bleManager: BLEManager
+	private var bleManager: BLEManager
 	@StateObject
-	var appState = AppState.shared
+	private var appState = AppState.shared
 
 	@State
 	private var columnVisibility = NavigationSplitViewVisibility.all
 	@State
 	private var selectedNode: NodeInfoEntity?
+	@State
+	private var favoriteNodes = 0
+	@State
+	private var onlineNodes = 0
+	@State
+	private var offlineNodes = 0
+	@State
+	private var loraNodes = 0
+	@State
+	private var loraSingleHopNodes = 0
+	@State
+	private var mqttNodes = 0
 	@State
 	private var searchText = ""
 
@@ -49,7 +61,8 @@ struct NodeList: View {
 	var body: some View {
 		NavigationSplitView(columnVisibility: $columnVisibility) {
 			List(selection: $selectedNode) {
-				suggestedList()
+				summary
+				suggestedList
 				nodeList(online: true)
 				nodeList(online: false)
 			}
@@ -89,6 +102,11 @@ struct NodeList: View {
 			)
 		}
 		.navigationSplitViewStyle(.balanced)
+		.onAppear {
+			Task {
+				await countNodes()
+			}
+		}
 		.onChange(of: searchText, initial: true) {
 			Task {
 				await updateFilter()
@@ -120,7 +138,89 @@ struct NodeList: View {
 	}
 
 	@ViewBuilder
-	private func suggestedList() -> some View {
+	private var summary: some View {
+		VStack(alignment: .leading, spacing: 4) {
+			Text("Online: \(onlineNodes) nodes")
+				.font(.system(size: 10, weight: .regular))
+				.foregroundColor(.gray)
+
+			VStack(alignment: .leading, spacing: 4) {
+				HStack(alignment: .center, spacing: 4) {
+					Image(systemName: "minus")
+						.font(.system(size: 10, weight: .light))
+						.foregroundColor(.gray)
+
+					Image(systemName: "star.circle")
+						.font(.system(size: 10, weight: .light))
+						.foregroundColor(.gray)
+
+					Text(String(favoriteNodes))
+						.font(.system(size: 10, weight: .light))
+						.foregroundColor(.gray)
+				}
+
+				HStack(alignment: .center, spacing: 4) {
+					Image(systemName: "minus")
+						.font(.system(size: 10, weight: .light))
+						.foregroundColor(.gray)
+
+					Image(systemName: "antenna.radiowaves.left.and.right.circle")
+						.font(.system(size: 10, weight: .light))
+						.foregroundColor(.gray)
+
+					Text(String(loraNodes))
+						.font(.system(size: 10, weight: .light))
+						.foregroundColor(.gray)
+
+					Spacer()
+						.frame(width: 4)
+
+					Image(systemName: "1.circle")
+						.font(.system(size: 10, weight: .light))
+						.foregroundColor(.gray)
+
+					Text(String(loraSingleHopNodes))
+						.font(.system(size: 10, weight: .light))
+						.foregroundColor(.gray)
+
+					Spacer()
+						.frame(width: 4)
+
+					Image(systemName: "plus.circle")
+						.font(.system(size: 10, weight: .light))
+						.foregroundColor(.gray)
+
+					Text(String(loraNodes - loraSingleHopNodes))
+						.font(.system(size: 10, weight: .light))
+						.foregroundColor(.gray)
+				}
+
+				HStack(alignment: .center, spacing: 4) {
+					Image(systemName: "minus")
+						.font(.system(size: 10, weight: .light))
+						.foregroundColor(.gray)
+
+					Image(systemName: "network")
+						.font(.system(size: 10, weight: .light))
+						.foregroundColor(.gray)
+
+					Text(String(mqttNodes))
+						.font(.system(size: 10, weight: .light))
+						.foregroundColor(.gray)
+				}
+			}
+
+			Divider()
+				.foregroundColor(.gray)
+
+			Text("Offline: \(offlineNodes) nodes")
+				.font(.system(size: 10, weight: .regular))
+				.foregroundColor(.gray)
+		}
+	}
+
+	@ViewBuilder
+	private var suggestedList: some View {
 		Section(
 			header: listHeader(
 				title: "In Case of Apocalypse",
@@ -217,6 +317,46 @@ struct NodeList: View {
 				node: node
 			)
 		}
+	}
+
+	private func countNodes() async {
+		var onlineNodes = 0
+		var offlineNodes = 0
+		var favoriteNodes = 0
+		var loraNodes = 0
+		var loraSingleHopNodes = 0
+		var mqttNodes = 0
+
+		for node in nodes {
+			if node.isOnline {
+				onlineNodes += 1
+
+				if node.favorite {
+					favoriteNodes += 1
+				}
+
+				if node.viaMqtt {
+					mqttNodes += 1
+				}
+				else {
+					loraNodes += 1
+
+					if node.hopsAway == 1 {
+						loraSingleHopNodes += 1
+					}
+				}
+			}
+			else {
+				offlineNodes += 1
+			}
+		}
+
+		self.onlineNodes = onlineNodes
+		self.offlineNodes = offlineNodes
+		self.favoriteNodes = favoriteNodes
+		self.loraNodes = loraNodes
+		self.loraSingleHopNodes = loraSingleHopNodes
+		self.mqttNodes = mqttNodes
 	}
 
 	private func updateFilter() async {
