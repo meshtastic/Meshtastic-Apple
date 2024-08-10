f87adad009c3b7186a2e73afaf5b8d5b60952af5
Benjamin Faershtein <119711889+RCGV1@users.noreply.github.com>
2024-08-09 23:02:24 -0700

    Created two App Intents and made a BLEManager singleton

---
+++ Meshtastic/AppIntents/MessageChannelIntent.swift
@@ -0,0 +1,8 @@
+//
+//  MessageChannelIntent.swift
+//  Meshtastic
+//
+//  Created by Benjamin Faershtein on 8/9/24.
+//
+
+import Foundation
---
+++ Meshtastic/AppIntents/SendWaypointIntent.swift
@@ -0,0 +1,8 @@
+//
+//  SendWaypointIntent.swift
+//  Meshtastic
+//
+//  Created by Benjamin Faershtein on 8/9/24.
+//
+
+import Foundation
