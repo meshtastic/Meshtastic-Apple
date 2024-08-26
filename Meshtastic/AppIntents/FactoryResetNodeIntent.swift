cfdcc5a2c3e97c74c0a2f4f96959afbee1b73377
Benjamin Faershtein <119711889+RCGV1@users.noreply.github.com>
2024-08-25 21:14:25 -0700

    Added Shortcuts provider and Shut Down, Reboot, and Factory Reset Intents. Also added logging for intent errors.

---
+++ Meshtastic/AppIntents/FactoryResetNodeIntent.swift
@@ -0,0 +1,41 @@
+//
+//  FactoryResetNodeIntent.swift
+//  Meshtastic
+//
+//  Created by Benjamin Faershtein on 8/25/24.
+//
+
+import Foundation
+import AppIntents
+
+struct FactoryResetNodeIntent: AppIntent {
+	static var title: LocalizedStringResource = "Factory Reset Node"
+	static var description: IntentDescription = "Perform a factory reset on the node you are connected to"
+	
+	func perform() async throws -> some IntentResult {
+		// Request user confirmation before performing the factory reset
+		try await requestConfirmation(result: .result(dialog: "Are you sure you want to factory reset the node?"),confirmationActionName: ConfirmationActionName
+			.custom(acceptLabel: "Factory Reset", acceptAlternatives: [], denyLabel: "Cancel", denyAlternatives: [], destructive: true))
+
+		// Ensure the node is connected
+		if !BLEManager.shared.isConnected {
+			throw AppIntentErrors.AppIntentError.notConnected
+		}
+
+		// Safely unwrap the connected node information
+		if let connectedPeripheralNum = BLEManager.shared.connectedPeripheral?.num,
+		   let connectedNode = getNodeInfo(id: connectedPeripheralNum, context: PersistenceController.shared.container.viewContext),
+		   let fromUser = connectedNode.user,
+		   let toUser = connectedNode.user {
+		   
+			// Attempt to send a factory reset command, throw an error if it fails
+			if !BLEManager.shared.sendFactoryReset(fromUser: fromUser, toUser: toUser) {
+				throw AppIntentErrors.AppIntentError.message("Failed to perform factory reset")
+			}
+		} else {
+			throw AppIntentErrors.AppIntentError.message("Failed to retrieve connected node or required data")
+		}
+//		
+		return .result()
+	}
+}
