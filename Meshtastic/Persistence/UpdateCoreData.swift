f8e1d0bfe39c4838db19df1fa62faad36da428ab
Garth Vander Houwen <garthvh@yahoo.com>
2024-09-12 13:52:47 -0700

    Save don't mqtt me bro

--- Meshtastic/Persistence/UpdateCoreData.swift
+++ Meshtastic/Persistence/UpdateCoreData.swift
@@ -598,6 +598,7 @@ func upsertLoRaConfigPacket(config: Config.LoRaConfig, nodeNum: Int64, sessionPa
 				newLoRaConfig.channelNum = Int32(config.channelNum)
 				newLoRaConfig.sx126xRxBoostedGain = config.sx126XRxBoostedGain
 				newLoRaConfig.ignoreMqtt = config.ignoreMqtt
+				newLoRaConfig.okToMqtt = config.configOkToMqtt
 				fetchedNode[0].loRaConfig = newLoRaConfig
 			} else {
 				fetchedNode[0].loRaConfig?.regionCode = Int32(config.region.rawValue)
@@ -615,6 +616,7 @@ func upsertLoRaConfigPacket(config: Config.LoRaConfig, nodeNum: Int64, sessionPa
 				fetchedNode[0].loRaConfig?.channelNum = Int32(config.channelNum)
 				fetchedNode[0].loRaConfig?.sx126xRxBoostedGain = config.sx126XRxBoostedGain
 				fetchedNode[0].loRaConfig?.ignoreMqtt = config.ignoreMqtt
+				fetchedNode[0].loRaConfig?.okToMqtt = config.configOkToMqtt
 				fetchedNode[0].loRaConfig?.sx126xRxBoostedGain = config.sx126XRxBoostedGain
 			}
 			if sessionPasskey != nil {
