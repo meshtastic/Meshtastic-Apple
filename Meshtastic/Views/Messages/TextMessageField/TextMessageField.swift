09bd3059d7c7502ad521a3f1dee26d6e3df9f86d
Garth Vander Houwen <garthvh@yahoo.com>
2024-09-22 07:49:55 -0700

    Imporoved length validation

--- Meshtastic/Views/Messages/TextMessageField/TextMessageField.swift
+++ Meshtastic/Views/Messages/TextMessageField/TextMessageField.swift
@@ -33,8 +33,9 @@ struct TextMessageField: View {
 					.onChange(of: typingMessage, perform: { value in
 						totalBytes = value.utf8.count
 						// Only mess with the value if it is too big
-						if totalBytes > Self.maxbytes {
+						while totalBytes > Self.maxbytes {
 							typingMessage = String(typingMessage.dropLast())
+							totalBytes = typingMessage.utf8.count
 						}
 					})
 					.keyboardType(.default)
