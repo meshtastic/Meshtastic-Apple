3a79e101825e26cc8062ab67e14a74b7c0f1f7bf
Garth Vander Houwen <garthvh@yahoo.com>
2025-01-01 11:56:25 -0800

    Use environment variables for default MQTT server connection info for client proxy

---
+++ Meshtastic/Extensions/EnvironmentValues.swift
@@ -0,0 +1,12 @@
+//
+//  EnvironmentValues.swift
+//  Meshtastic
+//
+//  Copyright(c) by Garth Vander Houwen on 1/1/25.
+//
+import SwiftUI
+
+extension EnvironmentValues {
+	@Entry var publicMqttUsername: String = "meshdev"
+	@Entry var publicMqttPsk: String = "large4cats"
+}
