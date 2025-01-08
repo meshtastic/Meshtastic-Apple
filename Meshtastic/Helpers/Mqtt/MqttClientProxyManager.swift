4062c677ab4349d178a641575e8401694df8ddae
Garth Vander Houwen <garthvh@yahoo.com>
2025-01-08 11:05:59 -0800

    Dont use environment variables

--- Meshtastic/Helpers/Mqtt/MqttClientProxyManager.swift
+++ Meshtastic/Helpers/Mqtt/MqttClientProxyManager.swift
@@ -47,8 +47,8 @@ class MqttClientProxyManager {
 			var password = node.mqttConfig?.password
 			if host == defaultServerAddress {
 
-				username = ProcessInfo.processInfo.environment["publicMqttUsername"]
-				password = ProcessInfo.processInfo.environment["publicMqttPsk"]
+				// username = ProcessInfo.processInfo.environment["publicMqttUsername"]
+				// password = ProcessInfo.processInfo.environment["publicMqttPsk"]
 				useSsl = false
 			}
 			let root = node.mqttConfig?.root?.count ?? 0 > 0 ? node.mqttConfig?.root : "msh"
