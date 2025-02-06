4d0312623db884bfd612da026a624934845e5347
Brian Floersch <gh123man@gmail.com>
2025-02-06 00:04:59 -0500

    Cleanup node list
    Added preview for NodeListItem, Added CoreData bindings, Align all icons, Deduplicate code for list items, Fix list view padding for tab bar transparency

---
+++ Meshtastic/CoreData/NodeInfoEntity+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  NodeInfoEntity+CoreDataClass.swift
+//  
+//
+//  Created by Brian Floersch on 2/5/25.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(NodeInfoEntity)
+public class NodeInfoEntity: NSManagedObject {
+
+}
---
+++ Meshtastic/CoreData/UserEntity+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  UserEntity+CoreDataClass.swift
+//  
+//
+//  Created by Brian Floersch on 2/5/25.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(UserEntity)
+public class UserEntity: NSManagedObject {
+
+}
