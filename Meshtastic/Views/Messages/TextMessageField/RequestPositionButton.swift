b0f1dbf355e0060e50ba2c3d9735593cc542e350
gitbisector <gitbisector@gmail.com>
2025-05-11 16:12:34 -0700

    Additional accessibilityLabels for VoiceOver users (take #3)

--- Meshtastic/Views/Messages/TextMessageField/RequestPositionButton.swift
+++ Meshtastic/Views/Messages/TextMessageField/RequestPositionButton.swift
@@ -6,6 +6,7 @@ struct RequestPositionButton: View {
 	var body: some View {
 		Button(action: action) {
 			Image(systemName: "mappin.and.ellipse")
+				.accessibilityLabel(NSLocalizedString("request_position", comment: "VoiceOver label for request position button"))
 				.symbolRenderingMode(.hierarchical)
 				.imageScale(.large)
 				.foregroundColor(.accentColor)
