0e3d99c4580bf69b6f5d7f91d86223286824ace4
Garth Vander Houwen <garthvh@yahoo.com>
2025-06-11 06:49:09 -0700

    Fix userlist connected node predicate

--- Meshtastic/Views/Helpers/MeshtasticLogo.swift
+++ Meshtastic/Views/Helpers/MeshtasticLogo.swift
@@ -11,26 +11,20 @@ struct MeshtasticLogo: View {
 	@Environment(\.colorScheme) var colorScheme
 
 	var body: some View {
-
 		#if targetEnvironment(macCatalyst)
 			VStack {
 				Image("logo-white")
 					.resizable()
-					.renderingMode(.template)
 					.foregroundColor(.accentColor)
 					.scaledToFit()
-					.offset(x: -15)
 			}
 			.padding(.bottom, 5)
 			.padding(.top, 5)
-			
 		#else
 			VStack {
 				Image(colorScheme == .dark ? "logo-white" : "logo-black")
 					.resizable()
-					.renderingMode(.template)
 					.scaledToFit()
-					.offset(x: -15)
 			}
 			.padding(.bottom, 5)
 		#endif
