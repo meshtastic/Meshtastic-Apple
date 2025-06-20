365699d1d41f12b1fdab62c47a0f0523a621bfa0
Garth Vander Houwen <garthvh@yahoo.com>
2025-06-20 07:12:54 -0700

    fix typo

--- Meshtastic/Tips/BluetoothTips.swift
+++ Meshtastic/Tips/BluetoothTips.swift
@@ -16,7 +16,7 @@ struct BluetoothConnectionTip: Tip {
 		Text("Connected Radio")
 	}
 	var message: Text? {
-		Text("Shows information for the Lora radio connected via bluetooth. You can swipe left to disconnect the radio and long press start the live activity.")
+		Text("Shows information for the Lora radio connected via bluetooth. You can swipe left to disconnect the radio and long press to start the live activity.")
 	}
 	var image: Image? {
 		Image(systemName: "flipphone")
