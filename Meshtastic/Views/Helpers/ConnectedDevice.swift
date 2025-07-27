2d80511d7d4c1c0a7293d6d1f0bed4aa497b68c9
Garth Vander Houwen <garthvh@yahoo.com>
2025-07-27 12:24:48 -0700

    Move color extention to the existing file, clean up the connected device widget spacing which seems to be oddly linted

--- Meshtastic/Views/Helpers/ConnectedDevice.swift
+++ Meshtastic/Views/Helpers/ConnectedDevice.swift
@@ -1,7 +1,7 @@
 /*
-Abstract:
-A view draws the indicator used in the upper right corner for views using BLE
-*/
+ Abstract:
+ A view that draws the indicator used in the upper right corner for views using BLE
+ */
 
 import SwiftUI
 
@@ -9,13 +9,12 @@ struct ConnectedDevice: View {
 	var bluetoothOn: Bool
 	var deviceConnected: Bool
 	var name: String
-
 	var mqttProxyConnected: Bool = false
 	var mqttUplinkEnabled: Bool = false
 	var mqttDownlinkEnabled: Bool = false
-		var mqttTopic: String = ""
+	var mqttTopic: String = ""
 	var phoneOnly: Bool = false
-
+	
 	var body: some View {
 		HStack {
 			if (phoneOnly && UIDevice.current.userInterfaceIdiom == .phone) || !phoneOnly {
@@ -69,16 +68,16 @@ struct ConnectedDevice: View {
 
 struct ConnectedDevice_Previews: PreviewProvider {
 	static var previews: some View {
-			VStack(alignment: .trailing) {
-				ConnectedDevice(bluetoothOn: true, deviceConnected: true, name: "MEMO", mqttProxyConnected: true)
-				ConnectedDevice(bluetoothOn: true, deviceConnected: true, name: "MEMO", mqttProxyConnected: false, mqttUplinkEnabled: true, mqttDownlinkEnabled: true)
-				ConnectedDevice(bluetoothOn: true, deviceConnected: true, name: "MEMO", mqttProxyConnected: true, mqttUplinkEnabled: true, mqttDownlinkEnabled: true, mqttTopic: "msh/US/2/e/#")
-				ConnectedDevice(bluetoothOn: true, deviceConnected: true, name: "MEMO", mqttProxyConnected: false, mqttUplinkEnabled: true, mqttDownlinkEnabled: false)
-				ConnectedDevice(bluetoothOn: true, deviceConnected: true, name: "MEMO", mqttProxyConnected: true, mqttUplinkEnabled: true, mqttDownlinkEnabled: false)
-				ConnectedDevice(bluetoothOn: true, deviceConnected: true, name: "MEMO", mqttProxyConnected: false, mqttUplinkEnabled: false, mqttDownlinkEnabled: true)
-				ConnectedDevice(bluetoothOn: true, deviceConnected: true, name: "MEMO", mqttProxyConnected: true, mqttUplinkEnabled: false, mqttDownlinkEnabled: true)
-				ConnectedDevice(bluetoothOn: true, deviceConnected: true, name: "MEMO", mqttProxyConnected: true)
-				ConnectedDevice(bluetoothOn: true, deviceConnected: false, name: "MEMO", mqttProxyConnected: false)
-			}.previewLayout(.fixed(width: 150, height: 275))
-		}
+		VStack(alignment: .trailing) {
+			ConnectedDevice(bluetoothOn: true, deviceConnected: true, name: "MEMO", mqttProxyConnected: true)
+			ConnectedDevice(bluetoothOn: true, deviceConnected: true, name: "MEMO", mqttProxyConnected: false, mqttUplinkEnabled: true, mqttDownlinkEnabled: true)
+			ConnectedDevice(bluetoothOn: true, deviceConnected: true, name: "MEMO", mqttProxyConnected: true, mqttUplinkEnabled: true, mqttDownlinkEnabled: true, mqttTopic: "msh/US/2/e/#")
+			ConnectedDevice(bluetoothOn: true, deviceConnected: true, name: "MEMO", mqttProxyConnected: false, mqttUplinkEnabled: true, mqttDownlinkEnabled: false)
+			ConnectedDevice(bluetoothOn: true, deviceConnected: true, name: "MEMO", mqttProxyConnected: true, mqttUplinkEnabled: true, mqttDownlinkEnabled: false)
+			ConnectedDevice(bluetoothOn: true, deviceConnected: true, name: "MEMO", mqttProxyConnected: false, mqttUplinkEnabled: false, mqttDownlinkEnabled: true)
+			ConnectedDevice(bluetoothOn: true, deviceConnected: true, name: "MEMO", mqttProxyConnected: true, mqttUplinkEnabled: false, mqttDownlinkEnabled: true)
+			ConnectedDevice(bluetoothOn: true, deviceConnected: true, name: "MEMO", mqttProxyConnected: true)
+			ConnectedDevice(bluetoothOn: true, deviceConnected: false, name: "MEMO", mqttProxyConnected: false)
+		}.previewLayout(.fixed(width: 150, height: 275))
+	}
 }
