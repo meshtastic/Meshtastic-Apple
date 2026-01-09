c4b7056783c199d6b6a5870f39b1d9710a5ca131
Garth Vander Houwen <garthvh@yahoo.com>
2026-01-08 20:12:09 -0800

    Remove UI kit method from waypoint form emoji picker

--- Meshtastic/Views/Nodes/MeshMap.swift
+++ Meshtastic/Views/Nodes/MeshMap.swift
@@ -120,16 +120,14 @@ struct MeshMap: View {
 				}
 				.sheet(item: $selectedWaypoint) { selection in
 					WaypointForm(waypoint: selection)
-						.padding()
+						.presentationDetents([.large])
 				}
 				.sheet(item: $editingWaypoint) { selection in
 					WaypointForm(waypoint: selection, editMode: true)
-						.padding()
+						.presentationDetents([.large])
 				}
 				.sheet(isPresented: $editingSettings) {
 					MapSettingsForm(traffic: $showTraffic, pointsOfInterest: $showPointsOfInterest, mapLayer: $selectedMapLayer, meshMap: $isMeshMap, enabledOverlayConfigs: $enabledOverlayConfigs)
-						.presentationDetents([.large])
-
 				}
 				.onChange(of: router.navigationState) {
 					guard case .map = router.navigationState.selectedTab else { return }
