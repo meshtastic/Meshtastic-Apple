6474240f4a29c06952bd7dc249b5f81049ec99ea
Garth Vander Houwen <garthvh@yahoo.com>
2026-01-15 10:48:00 -0800

    Increase connection timeout

--- Meshtastic/Accessory/Accessory Manager/AccessoryManager+Connect.swift
+++ Meshtastic/Accessory/Accessory Manager/AccessoryManager+Connect.swift
@@ -48,7 +48,7 @@ extension AccessoryManager {
 			}
 			
 			// Step 1: Setup the connection
-			Step(timeout: .seconds(2)) { @MainActor _ in
+			Step(timeout: .seconds(5)) { @MainActor _ in
 				Logger.transport.info("🔗👟[Connect] Step 1: connection to \(device.id, privacy: .public)")
 				do {
 					let connection: Connection
